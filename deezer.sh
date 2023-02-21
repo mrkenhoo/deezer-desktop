e353d2dd59b3c24c17b7f5e2b81d02d9d4b51678
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2023-02-21 12:55:55 +0000

    Make code  POSIX-compliant

--- deezer.sh
+++ deezer.sh
@@ -18,7 +18,7 @@ tmpdir="$srcdir/tmp"
 
 help()
 {
-    echo "Usage: ${0} [OPTIONS]
+    echo "Usage: `basename $0` [OPTIONS]
 
 Options:
   --build-package,-b    Build package for $pkgname
@@ -34,54 +34,59 @@ main()
 {
     tmpfile=$(mktemp)
 
-    dialog --menu "Choose your distribution" 10 35 1 \
-        1 openSUSE \
-        2 RHEL \
-        3 Debian 2> $tmpfile
+    zenity --list \
+           --title="Choose your distribution" \
+           --column=ID --column=Distribution \
+           opensuse "openSUSE" \
+           debian "Debian GNU/Linux" \
+           rhel "Red Hat Enterprise Linux" > $tmpfile
 
     case "`cat $tmpfile`" in
-        1) export PACKAGE_TYPE="rpm" LINUX_DISTRIBUTION="opensuse";;
-        2) export PACKAGE_TYPE="rpm" LINUX_DISTRIBUTION="rhel";;
-        3) export PACKAGE_TYPE="deb" LINUX_DISTRIBUTION="debian";;
+        opensuse) export PACKAGE_TYPE="rpm" LINUX_DISTRIBUTION="opensuse";;
+        rhel) export PACKAGE_TYPE="rpm" LINUX_DISTRIBUTION="rhel";;
+        debian) export PACKAGE_TYPE="deb" LINUX_DISTRIBUTION="debian";;
     esac
 
-    return $? && clear
+    return $?
 }
 
 prepare()
 {
-    if [ ! -z $LINUX_DISTRIBUTION ]
+    if test ! -z $LINUX_DISTRIBUTION
     then
-        if [ $LINUX_DISTRIBUTION = "opensuse" ]
+        if test $LINUX_DISTRIBUTION = "opensuse"
         then
             sudo zypper install -y ImageMagick curl nodejs npm p7zip-full || return 1
-        elif [ $LINUX_DISTRIBUTION = "rhel" ]
+        elif test $LINUX_DISTRIBUTION = "rhel"
         then
             sudo dnf install -y ImageMagick curl nodejs npm p7zip-full || return 1
-        elif [ $LINUX_DISTRIBUTION = "debian" ]
+        elif test $LINUX_DISTRIBUTION = "debian"
         then
             sudo apt install -y imagemagick curl nodejs npm p7zip-full || return 1
-         else
-             echo "Unknown distribution: $LINUX_DISTRIBUTION" && exit 1
-         fi
+        else
+            echo "Unknown distribution: $LINUX_DISTRIBUTION" && exit 1
+        fi
     else
         echo "No distribution chosen, exiting..." && exit 1
     fi
 
-    [ ! -d "$srcdir" ] && mkdir -pv "$srcdir"
-    [ ! -d "$tmpdir" ] && mkdir -pv "$tmpdir"
-    [ ! -d "$pkgdir" ] && mkdir -pv "$pkgdir"
-    [ ! -d "$pkgdir/$_pkgname" ] && mkdir -pv "$pkgdir/$_pkgname"
+    test test ! -d "$srcdir" && mkdir -pv "$srcdir"
+    test test ! -d "$tmpdir" && mkdir -pv "$tmpdir"
+    test test ! -d "$pkgdir" && mkdir -pv "$pkgdir"
+    test test ! -d "$pkgdir/$_pkgname" && mkdir -pv "$pkgdir/$_pkgname"
 
     cd "$srcdir" || return 1
     sudo npm install -g electron@^13 --unsafe-perm=true
     sudo npm install -g --engine-strict @electron/asar
     sudo npm install -g prettier
 
-    if [ "$PACKAGE_TYPE" = "deb" ]
+    if test "$PACKAGE_TYPE" = "deb"
     then
-        [ ! -d "$pkgdir/DEBIAN/" ] && mkdir -p "$pkgdir/$_pkgname/DEBIAN/"
-        [ ! -f "$pkgdir/DEBIAN/control" ] && echo "Source: $_pkgname
+        test ! -d  "$pkgdir/DEBIAN/" && \
+            mkdir -p "$pkgdir/$_pkgname/DEBIAN/"
+
+        test ! -f "$pkgdir/DEBIAN/control" && \
+            echo "Source: $_pkgname
 Package: $pkgname
 Version: $pkgver-$pkgrel
 Depends: nodejs
@@ -91,20 +96,34 @@ Architecture: $arch
 Essential: no
 Maintainer: Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
 Copyright: Copyright (c) 2006-2022 Deezer S.A.
-Description: Deezer audio streaming service" | tee "$pkgdir/$_pkgname/DEBIAN/control" > /dev/null
+Description: Deezer audio streaming service" | \
+                tee "$pkgdir/$_pkgname/DEBIAN/control" > /dev/null
 
-        mkdir -p "$pkgdir/$_pkgname/usr/share/deezer"
-        mkdir -p "$pkgdir/$_pkgname/usr/share/applications"
-        mkdir -p "$pkgdir/$_pkgname/usr/bin/"
+        mkdir -pv "$pkgdir/$_pkgname/usr/share/deezer"
+        mkdir -pv "$pkgdir/$_pkgname/usr/share/applications"
+        mkdir -pv "$pkgdir/$_pkgname/usr/bin/"
 
-        [ ! -f "$pkgdir/$_pkgname/usr/bin/" ] && echo 'exec electron /usr/share/deezer/app.asar "$@"' | tee "$pkgdir/$_pkgname/usr/bin/deezer" > /dev/null
+        if test ! -f "$pkgdir/$_pkgname/usr/bin/deezer"
+        then
+            echo 'exec electron /usr/share/deezer/app.asar "$@"' | \
+                tee "$pkgdir/$_pkgname/usr/bin/deezer" > /dev/null
+        fi
 
         for size in 16 32 48 64 128 256; do
-            [ ! -d "$size" ] && mkdir -p "$pkgdir/$_pkgname/usr/share/icons/hicolor/${size}x${size}/apps/"
+            if test test ! -d  "$size"
+            then
+                mkdir -p "$pkgdir/$_pkgname/usr/share/icons/hicolor/${size}x${size}/apps/"
+            fi
         done
-    elif [ "$PACKAGE_TYPE" = "rpm" ]
+    elif test "$PACKAGE_TYPE" = "rpm"
     then
-        echo "Not implemented yet" && return 1
+        electron-installer-redhat \
+            --src "$pkgdir/$_pkgname" \
+            --dest "$srcdir" \
+            --arch "$arch" \
+            --options.productName "$pkgname" \
+            --options.icon "$pkgdir/$_pkgname/usr/share/icons/hicolor/apps/deezer.png" \
+            --options.desktopTemplate "src/deezer.desktop.ejs"
     fi
 
     return $?
@@ -112,41 +131,41 @@ Description: Deezer audio streaming service" | tee "$pkgdir/$_pkgname/DEBIAN/con
 
 build()
 {
-    [ ! -f "$tmpdir/$pkgname-$pkgver-setup.exe" ] && \
+    test ! -f "$tmpdir/$pkgname-$pkgver-setup.exe" && \
         curl -fSL "https://www.deezer.com/desktop/download/artifact/win32/x86/$pkgver" \
             -o "$tmpdir/$pkgname-$pkgver-setup.exe"
 
-    [ ! -f "$tmpdir/app-32.7z" ] && \
+     ! -f "$tmpdir/app-32.7z" && \
         7z x -so "$tmpdir/$pkgname-$pkgver-setup.exe" "\$PLUGINSDIR/app-32.7z" > "$tmpdir/app-32.7z"
 
-    [ ! -d "$tmpdir/deezer" ] && \
+     test ! -d  "$tmpdir/deezer" && \
         7z x -bsp0 -bso0 -y "$tmpdir/app-32.7z" -o"$tmpdir/deezer"
 
     convert "$tmpdir/deezer/resources/win/app.ico" "$tmpdir/deezer/resources/win/deezer.png"
 
-    [ -d "$tmpdir/deezer/resources/" ] && \
+    test -d "$tmpdir/deezer/resources/" && \
         cd "$tmpdir/deezer/resources/"
 
-    [ -d "app" ] && rm -rf "app" || \
-        [ -d "npm_temp" ] && rm -rf "npm_temp"
+    test -d "app" && rm -rf "app" || \
+        test -d "npm_temp" && rm -rf "npm_temp"
 
     asar extract "app.asar" "app" && \
-        [ ! -d "app/resources/linux" ] && \
+         test ! -d  "app/resources/linux" && \
             mkdir -p "app/resources/linux/"
 
-    [ -d "app/node_modules/@nodert" ] && \
+    test -d "app/node_modules/@nodert" && \
         rm -r "app/node_modules/@nodert"
 
-    [ ! -d "npm_temp" ] && mkdir "npm_temp" && \
+     test ! -d  "npm_temp" && mkdir "npm_temp" && \
         npm install --prefix npm_temp mpris-service
 
     for d in npm_temp/node_modules/*
     do
-        [ ! -d "app/node_modules/`basename $d`" ] && \
+         test ! -d  "app/node_modules/`basename $d`" && \
             mv "$d" "app/node_modules/"
     done
 
-    [ -d "app/resources/linux" ] && \
+    test -d "app/resources/linux" && \
         install -Dm644 "win/systray.png" "app/resources/linux/"
 
     cd "app" && prettier --loglevel error --write "build/*.js"
@@ -158,11 +177,11 @@ build()
     patch --forward --strip=1 --input="../../../../start-hidden-on-tray.patch"
 
     cd .. && \
-        [ -f "app/node_modules/abstract-socket/build/node_gyp_bins/python3" ] && \
+        test -f "app/node_modules/abstract-socket/build/node_gyp_bins/python3" && \
             rm "app/node_modules/abstract-socket/build/node_gyp_bins/python3" || \
                 asar pack "app" "app.asar"
 
-    if [ $PACKAGE_TYPE = "deb" ]
+    if test $PACKAGE_TYPE = "deb"
     then
         install -Dm644 "$tmpdir/deezer/resources/app.asar" "$pkgdir/$_pkgname/usr/share/deezer/"
         install -Dm644 "$tmpdir/deezer/resources/win/deezer-0.png" "$pkgdir/$_pkgname/usr/share/icons/hicolor/16x16/apps/deezer.png"
@@ -176,18 +195,12 @@ build()
 
         cd "$pkgdir/$_pkgname" && \
             dpkg-deb -v --build "`pwd`" && \
-                [ -x "`command -v update-desktop-database`" ] && \
+                test -x "`command -v update-desktop-database`" && \
                     update-desktop-database --quiet || \
                         echo "Build the cache database of MIME types yourself to handle the '$pkgname://' protocol"
-    elif [ $PACKAGE_TYPE = "rpm" ]
+    elif test $PACKAGE_TYPE = "rpm"
     then
-        electron-installer-redhat \
-            --src $pkgdir/$_pkgname \
-            --dest $srcdir \
-            --arch $arch \
-            --options.productName $pkgname \
-            --options.icon $pkgdir/$_pkgname/usr/share/icons/hicolor/apps/deezer.png \
-            --options.desktopTemplate src/deezer.desktop.ejs
+        echo "Not yet implemented" && return 1
     fi
 
     return $?
@@ -197,11 +210,11 @@ install_deezer()
 {
     for f in pkg/
     do
-        if [ "$PACKAGE_TYPE" = "deb" ]
+        if test "$PACKAGE_TYPE" = "deb"
         then
-            if [ -f "$pkgdir/$_pkgname.deb" ]
+            if test -f "$pkgdir/$_pkgname.deb"
             then
-                if [ ! -x "`command -v deezer`" ]
+                if test ! -x "`command -v deezer`"
                 then
                     printf "Package found: $pkgdir/$_pkgname.deb\nVersion: $pkgver\nRelease version: $pkgrel\n"
                     read -p "Do you want to install it now? [Y/N] " installPackage
@@ -210,12 +223,12 @@ install_deezer()
                         sudo dpkg -i "$pkgdir/$_pkgname.deb"
                     ;;
                     esac
-                elif [ "`dpkg -s deezer | grep Version | cut -d " " -f 2`" = "$pkgver-$pkgrel" ]
+                elif test "`dpkg -s deezer | grep Version | cut -d " " -f 2`" = "$pkgver-$pkgrel"
                 then
                     echo "You are already using the latest version of $pkgname ($pkgver-$pkgrel)."
                 fi
             fi
-        elif [ "$PACKAGE_TYPE" = "rpm" ]
+        elif "$PACKAGE_TYPE" = "rpm"
         then
             echo "Not yet implemented" && return 1
         fi
@@ -228,11 +241,11 @@ uninstall_deezer()
 {
     for f in pkg/
     do
-        if [ "$PACKAGE_TYPE" = "deb" ]
+        if test "$PACKAGE_TYPE" = "deb"
         then
-            if [ -f "$pkgdir/$_pkgname.deb" ]
+            if test -f "$pkgdir/$_pkgname.deb"
             then
-                if [ -x "`command -v deezer`" ]
+                if test -x "`command -v deezer`"
                 then
                     printf "Package found: $pkgdir/$_pkgname.deb\nVersion: $pkgver\nRelease version: $pkgrel\n"
                     read -p "Do you want to uninstall it now? [Y/N] " installPackage
@@ -241,36 +254,36 @@ uninstall_deezer()
                         sudo apt purge --autoremove $pkgname
                     ;;
                     esac
-                elif [ "`dpkg -s deezer | grep Version | cut -d " " -f 2`" = "$pkgver-$pkgrel" ]
+                elif test "`dpkg -s deezer | grep Version | cut -d " " -f 2`" = "$pkgver-$pkgrel"
                 then
                     echo "You are already using the latest version of $pkgname ($pkgver-$pkgrel)."
                 fi
             fi
-        elif [ $PACKAGE_TYPE = "rpm" ]
+        elif $PACKAGE_TYPE = "rpm"
         then
             echo "Not yet implemented" && return 1
         fi
     done
+    
+    return $?
 }
 
 cleanup()
 {
-    find $tmpdir/* -exec rm -rfv {} \;
-
-    return $?
+    find $tmpdir/* -exec rm -rfv {} \;; return $?
 }
 
-[ "$#" -eq "0" ] && help && exit $?
+test $# -eq 0 && help && exit 1
 
-while [ "$#" -eq "1" ]
+while getopts 'biuch' arg
 do
-    case "$1" in
-        --build-deb-package|-b) main && prepare && build; exit $?;;
-    	--install|-i) main && install_deezer; exit $?;;
-        --uninstall|-u) main && uninstall_deezer; exit $?;;
-        --cleanup|-c) cleanup; exit $?;;
-    	--help|-h) help; exit $?;;
-    	*) [ ! -z $1 ] && printf "ERROR: Unknown parameter: $1\n\n" && help || help; exit $?;;
+    case $arg in
+        b) main && prepare && build; exit $?;;
+    	i) main && install_deezer; exit $?;;
+        u) main && uninstall_deezer; exit $?;;
+        c) cleanup; exit $?;;
+        h) help; exit 1;;
+        ?) help; exit 1;;
     esac
     shift
 done
